b947a64094f390ef8c4afddc027b786a866cdf9e
Emmanuella <105435095+OD2022@users.noreply.github.com>
2024-05-29 22:16:57 +0000

    Updated UI to accept multiple input

--- streamlit/user_interface.py
+++ streamlit/user_interface.py
@@ -1,16 +1,51 @@
 import streamlit as st
 
-st.title("A Nutririon Advisor for Sickle Cell Disease")
-st.sidebar.title("Eliza Cares")
-st.sidebar.image('logo.png')
 
-st.radio('I have eaten today',['Yes','No'])
-st.radio('Pick your gender',['Male','Female'])
+st.image('logo2.png')
+st.title("IOEAs Nutrition Advisor for Ghanaians with Sickle Cell Disease")
+
+
+st.sidebar.title("Quantity Selector")
+
+
+
+st.radio('Have you eaten in the last 24 hours?',['Yes','No'])
+eaten_food_options = ['Jollof Rice', 'Grilled Chicken', 'Fried Chicken', 'Aprapransa']
+selected_eaten_options = st.multiselect('What You have eaten in the past 24 hours?', eaten_food_options)
+
+desired_food_options = ['Jollof Rice', 'Grilled Chicken', 'Fried Chicken', 'Aprapransa']
+selected_desired_options = st.multiselect('What are you planning to eat now?', desired_food_options)
+
+# Conditional appearance based on selection status
+for food in selected_eaten_options:
+    slider_label = f'Quantity in Grams for {food + ' eaten'}'
+    amount = st.sidebar.slider(slider_label, 0, 10000)
+
+
+for food in selected_desired_options:
+    slider_label = f'Select Quantity in Grams for {food + 'desired'}'
+    amount = st.sidebar.slider(slider_label, 0, 10000)
+
+
+st.radio('What is your gender?',['Male','Female'])
 st.selectbox('Pick your Age Group',['1-18','18-70', '70+'])
-st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
-st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
-st.slider('Pick a number', 0,50)
+st.radio("Select the system providing your report", ['Detailed', 'Summary', 'Explanatory'])
+st.button('Get Advice')
 
 
-st.progress(10)
-st.button('Submit')
+
+
+st.markdown(
+    f"""
+    <style>
+        img {{
+            border-radius: 80%;
+            overflow: hidden;
+            width: 200px;
+            height: 200px;
+           
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
